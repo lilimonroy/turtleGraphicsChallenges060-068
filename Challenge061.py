628a9ba212694e78a3c58c2cf130511f8bc1f5d2
Lili Monroy <galadriel@comunidad.unam.mx>
2022-01-26 18:18:41 -0800

    fetch:Challenges60-63

---
+++ Challenge061.py
@@ -0,0 +1,13 @@
+#----------* CHALLENGE 61 *----------
+#Draw a triangule.
+
+import turtle
+
+scr = turtle.Screen()
+
+turtle.shape("turtle")
+
+for i in range (0,3):
+    turtle.forward(100)
+    turtle.left(120)
+turtle.exitonclick()
