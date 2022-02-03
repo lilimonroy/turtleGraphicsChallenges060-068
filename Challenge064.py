15a41f1eb9e8b27bd7db5e433cd2e59d75a91021
Lili Monroy <galadriel@comunidad.unam.mx>
2022-02-02 17:51:55 -0800

    fetch: Challenge064

---
+++ Challenge064.py
@@ -0,0 +1,10 @@
+#----------* CHALLENGE 63 *----------
+# Draw a five-pointed star.
+
+import turtle
+
+for i in range (0,5):
+    turtle.forward(100)
+    turtle.right(144)
+        
+turtle.exitonclick()
